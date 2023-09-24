bf1fd421e26204bf7b9955d07e538968e3aa0a4a
Marie Claudelle YIMGA TCHANGO <marie.claudelle.yimga.tchango@procomm-mmc.com>
2023-09-24 20:23:22 +0200

    fin du plateau d'échec

---
+++ chess-app/src/App.tsx
@@ -0,0 +1,11 @@
+ import './App.css';
+ import ChessBoard from './components/ChessBoard';
+function App() {
+  return (
+    <div id="app">
+      <ChessBoard/>
+    </div>
+  );
+}
+
+export default App;
